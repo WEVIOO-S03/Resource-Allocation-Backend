52852b10f16e7a56f62790ebbbfb182b1d596e27
manelaskry <manlaskry8@gmail.com>
2025-03-25 00:46:05 +0100

    Add and configure CORS support with bundles.php, cors.yaml, and nelmio_cors.yaml in config/packages

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
