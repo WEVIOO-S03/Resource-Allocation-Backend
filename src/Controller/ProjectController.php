7b672c740313f03549ac844acf43d8385f8c31a9
manelaskry <manlaskry8@gmail.com>
2025-04-19 14:01:45 +0200

    Enhance Project and User entities with resource management functionality, including methods to add and remove resources, and update ProjectController to return resource details in project responses.

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -21,9 +21,54 @@ class ProjectController extends AbstractController
                 'code' => $project->getCode(),
                 'name' => $project->getName(),
                 'requiredSkills' => $project->getRequiredSkills(),
+                'resources' => array_map(function($resource) {
+                    return [
+                        'id' => $resource->getId(),
+                        'fullName' => $resource->getFullName(),
+                        'position' => $resource->getPosition(),
+                        'skills' => $resource->getSkills(),
+                        'occupationRate' => $resource->getOccupationRate(),
+                        'pole' => $resource->getPole() ? [
+                            'id' => $resource->getPole()->getId(),
+                            'name' => $resource->getPole()->getName()
+                        ] : null
+                    ];
+                }, $project->getResources()->toArray())
             ];
         }
 
         return $this->json($formattedProjects);
+    }
+
+    #[Route('/api/projects/{id}', name: 'api_project', methods: ['GET'])]
+    public function getProject(ProjectRepository $projectRepository, int $id): JsonResponse
+    {
+        $project = $projectRepository->find($id);
+
+        if (!$project) {
+            return $this->json(['error' => 'Project not found'], 404);
+        }
+
+        $formattedProject = [
+            'id' => $project->getId(),
+            'code' => $project->getCode(),
+            'name' => $project->getName(),
+            'requiredSkills' => $project->getRequiredSkills(),
+            'resources' => array_map(function($resource) {
+                return [
+                    'id' => $resource->getId(),
+                    'fullName' => $resource->getFullName(),
+                    'position' => $resource->getPosition(),
+                    'skills' => $resource->getSkills(),
+                    'occupationRate' => $resource->getOccupationRate(),
+                    'pole' => $resource->getPole() ? [
+                        'id' => $resource->getPole()->getId(),
+                        'name' => $resource->getPole()->getName()
+                    ] : null
+                ];
+            }, $project->getResources()->toArray())
+        ];
+
+        return $this->json($formattedProject);
     }
 }
